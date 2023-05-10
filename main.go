c123c291f153002512133e3440cf65084e943991
Arsh Suri <gunmala@gunmalas-MacBook-Pro.local>
2023-05-10 12:44:53 +0530

    added cache hit/miss logic

--- main.go
+++ main.go
@@ -1,25 +1,33 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+	"os"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 func main() {
 	fmt.Println("Starting server")
 
-	http.HandleFunc("/api", Handler)
+	api := NewAPI()
+
+	http.HandleFunc("/api", api.Handler)
 	http.ListenAndServe(":8080", nil)
 
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func (a *api) Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("In the handler")
 
 	q := r.URL.Query().Get("q")
-	data, err := getData(q)
+	data, cacheHit, err := a.getData(r.Context(), q)
 	if err != nil {
 		fmt.Printf("error calling data source: %v\n", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -27,7 +35,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	resp := APIResponse{
-		Cache: false,
+		Cache: cacheHit,
 		Data:  data,
 	}
 
@@ -38,25 +46,76 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func getData(q string) ([]NominatimResponse, error) {
+func (a *api) getData(ctx context.Context, q string) ([]NominatimResponse, bool, error) {
 
-	//remove space when entering value
-	escapedQ := url.PathEscape(q)
-	address := fmt.Sprintf("https://nominatim.openstreetmap.org/search?q=%s&format=json", escapedQ)
+	//is the query cached? [is the data present in cache?]
+	value, err := a.cache.Get(ctx, q).Result()
 
-	//extract value out of the url [ http.get ]
-	resp, err := http.Get(address)
-	if err != nil {
-		return nil, err
+	if err == redis.Nil {
+		//call external DS
+		escapedQ := url.PathEscape(q)
+		address := fmt.Sprintf("https://nominatim.openstreetmap.org/search?q=%s&format=json", escapedQ)
+
+		//extract value out of the url [ http.get ]
+		resp, err := http.Get(address)
+		if err != nil {
+			return nil, false, err
+		}
+
+		data := make([]NominatimResponse, 0)
+		//read that resp
+		err = json.NewDecoder(resp.Body).Decode(&data)
+		if err != nil {
+			return nil, false, err
+		}
+
+		b, err := json.Marshal(data)
+		if err != nil {
+			return nil, false, err
+		}
+
+		//Set the value
+		err = a.cache.Set(ctx, "q", bytes.NewBuffer(b).Bytes(), time.Second*15).Err()
+		if err != nil {
+			panic(err)
+		}
+
+		//return the value
+		return data, false, nil
+
+	} else if err != nil {
+		return nil, false, err
+	} else {
+		//build response
+		data := make([]NominatimResponse, 0)
+
+		err := json.Unmarshal(bytes.NewBufferString(value).Bytes(), &data)
+		if err != nil {
+			return nil, false, err
+		}
+
+		//return response
+		return data, true, nil
+
 	}
+}
 
-	data := make([]NominatimResponse, 0)
-	//read that resp
-	err = json.NewDecoder(resp.Body).Decode(&data)
-	if err != nil {
-		return nil, err
+type api struct {
+	cache *redis.Client
+}
+
+func NewAPI() *api {
+	redisAddress := fmt.Sprintf("%s:6379", os.Getenv("REDIS_URL"))
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     redisAddress,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	return &api{
+		cache: rdb,
 	}
-	return data, nil
 }
 
 //we need to register a response from the PAI, also check whether its a cache hit or miss
